035e655c1d80f69fb0a31c01775630c642e365eb
Novia Adelia <87015833+viadelia12@users.noreply.github.com>
2023-03-13 11:16:48 +0700

    Minor change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send("Hi, i'm Novia!");
 });
 
 const PORT = process.env.PORT || 8080;
